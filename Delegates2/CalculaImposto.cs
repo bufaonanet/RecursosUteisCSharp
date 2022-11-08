e3558b6b9c6fa8652e0fbbbf32927b5392e59f4c
bufaonanet <douglas.bufaonanet@outlook.com>
2022-11-08 16:34:58 -0300

    mais sobre deletages Func Action

---
+++ Delegates2/CalculaImposto.cs
@@ -0,0 +1,44 @@
+﻿namespace Delegates2;
+
+
+
+public delegate double RetornaCobranca(double montante, bool isento);
+
+public class CalculaImposto
+{
+
+    public double RetornaCobrancaICMS(double montante, bool isento)
+    {
+        double valorCobrado = 0;
+
+        if (!isento)
+            valorCobrado = montante * 0.20;
+
+        return valorCobrado;
+    }
+
+    public double RetornaCobrancaIRRF(double montante, bool isento)
+    {
+        double valorCobrado = 0;
+
+        if (!isento)
+        {
+            if (montante > 2000 && montante <= 4000)
+                valorCobrado = montante * 0.15;
+
+            if (montante > 4000)
+                valorCobrado = montante * 0.35;
+        }
+        return valorCobrado;
+    }
+
+    public int MetodoComretorno(bool parametro)
+    {
+        return 0;
+    }
+
+    public void MetodoVoid(bool parametro)
+    {
+        
+    }
+}
---
+++ Delegates2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Delegates2;
+
+CalculaImposto imposto = new();
+RetornaCobranca aplica_icms = imposto.RetornaCobrancaICMS;
+RetornaCobranca aplica_irrf = imposto.RetornaCobrancaIRRF;
+
+var metodoSemRetorno = imposto.MetodoComretorno;
+var metodovoid = imposto.MetodoVoid;
+
+
+double icms = aplica_icms(300,false);
+
+double irrf = aplica_irrf(300,false);
+
+Console.WriteLine($"icms = {icms}");
+Console.WriteLine($"irrf = {irrf}");
